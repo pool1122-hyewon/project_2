5434711f8db1160ee1365153bde39b11121264d3
pool1122 <77139662+pool1122@users.noreply.github.com>
2021-04-01 18:31:37 +0900

    project_2

---
+++ js/common.js
@@ -0,0 +1,34 @@
+$(function(){      
+
+    var delta, num=$(window).width();
+    
+    $(window).on('mousewheel DOMMouseScroll',function(e){
+        delta = e.originalEvent.wheelDelta || -e.originalEvent.detail;
+        
+        if(delta<0){
+            //down
+            if(-($('.page_02').width() - $(window).width() )< num) num-=100;
+        }else{
+            //up
+            if($(window).width() > num) num+=100;
+        }
+        $('.page_02').css({transform:'translateX('+num+'px)'});
+
+        console.log($('.spot_04').offset().left)
+
+        //콘텐츠 나타남
+        $('.in').each(function(){
+            if($(this).offset().left < $(window).width() ){
+                $(this).addClass('active');
+            }
+        });
+        
+    });
+    
+    $('.article_06 a').on('click',function(){
+        $('.page_02').css({transform:'translateX(100vw)'});
+    })
+
+    
+
+});
